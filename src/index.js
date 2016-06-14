df70cc3b7546654366db9824c6740d1d7d76ea75
Benjamin Toll <benjam72@yahoo.com>
2016-06-13 21:42:37 -0400

    Added log levels
    Removed logger.disable and logger.enable in favor of setting log levels.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 'use strict';
 
-const logger = {};
+let format = require('./format/base');
+
+// Internal Field Separator.
+const IFS = '|';
+
+const logLevels = {
+    RAW: 1,
+    INFO: 2,
+    WARN: 4,
+    ERROR: 8,
+    FATAL: 16,
+    DEBUG: 32
+};
+
+// We're repeating some properties here for readability!
+logLevels.ALL = logLevels.RAW +
+                logLevels.INFO +
+                logLevels.WARN +
+                logLevels.ERROR +
+                logLevels.FATAL;
+
+logLevels.ERRORS = logLevels.WARN +
+                   logLevels.ERROR;
+
+logLevels.ERRORS_ALL = logLevels.WARN +
+                       logLevels.ERROR +
+                       logLevels.FATAL;
+
+logLevels.INFO_ALL = logLevels.RAW +
+                     logLevels.INFO;
+
 // TODO
 const preprocess = () => '';
 const postprocess = () => '';
@@ -11,13 +41,16 @@ const aliases = {
     success: 'log'
 };
 
-let format = require('./format/base');
+// Default to logging all info and all errors except FATAL.
+let logLevel = logLevels.INFO_ALL + logLevels.ERRORS;
 let wrapped = console || {};
-let disabled = false;
+
+const checkLogLevel = level =>
+    logLevel & logLevels[level];
 
 const invoke = methodName =>
     function () {
-        if (disabled) {
+        if (!checkLogLevel(methodName.toUpperCase())) {
             return;
         }
 
@@ -30,11 +63,13 @@ const invoke = methodName =>
             // Check first if it's an alias so an actual underlying implementation is called!
             wrapped[aliases[methodName] || methodName].apply(wrapped, [format.prelog(methodName)]
                 .concat(Array.from(arguments))
-                .concat([ format.postlog(methodName) ]));
+                .concat([format.postlog(methodName)]));
         }
 
         postprocess(methodName);
     };
+
+const logger = {};
 
 for (const methodName of Object.keys(wrapped)) {
     logger[methodName] = invoke(methodName);
@@ -44,14 +79,35 @@ for (const alias of Object.keys(aliases)) {
     logger[alias] = invoke(alias);
 }
 
-// Allow access to the underlying wrapped logger object.
-logger.__get = () => wrapped;
-logger.disable = () => disabled = true;
-logger.enable = () => disabled = false;
-// TODO
-logger.logLevel = () => {};
+/**
+ * level === Number or a String.
+ *
+ * For those who know what they're doing, they can simply pass the bit value as a Number.
+ *
+ *      logger.setLogLevel(14);
+ *
+ * Else, it supports passing a String of one or more log level values delimited by the IFS:
+ *
+ *      logger.setLogLevel('ALL');
+ *      logger.setLogLevel('ERRORS|INFO_ALL|DEBUG');
+ *
+ * The .reduce will simply add up the bit values.
+ */
+logger.setLogLevel = level =>
+    logLevel = typeof level !== 'number' ?
+        level.indexOf(IFS) > -1 ?
+            level.split(IFS).reduce((acc, curr) => {
+                acc += logLevels[curr];
+                return acc;
+            }, 0) :
+            logLevels[level] :
+        level;
+
 logger.setFormat = f => format = require(`./format/${f}`);
 logger.wrap = target => wrapped = target;
+
+// Allow access to the underlying wrapped logger object.
+logger.__get = () => wrapped;
 
 module.exports = logger;
 
